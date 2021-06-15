d8c59056e2e490f6e4bd513583cc594e36ff6532
Noman <76186680+kawanoman@users.noreply.github.com>
2021-06-15 23:02:51 +0200

    Add files via upload

---
+++ Pyplot_Scatter.py
@@ -0,0 +1,16 @@
+//
+//  Pyplot_Scatter.py
+//  
+//
+//  Created by Kawa Noman on 15.06.21.
+//
+
+import Foundation
+
+
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3, 4])
+plt.ylabel('some numbers')
+plt.show()
+
+
