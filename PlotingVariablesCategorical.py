3d000317142545559e0fa5a5f31f28f8837b98ea
Kawa Noman <76186680+kawanoman@users.noreply.github.com>
2021-06-26 21:55:38 +0200

    Create PlotingVariablesCategorical.py

---
+++ PlotingVariablesCategorical.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+names = ['group_a', 'group_b', 'group_c']
+values = [1, 10, 100]
+
+plt.figure(figsize=(8, 3))
+
+plt.subplot(131)
+plt.bar(names, values)
+plt.subplot(132)
+plt.scatter(names, values ,  c="g", alpha=0.5, marker=r'$\clubsuit$',
+            label="Luck")
+plt.legend(loc='upper left')
+plt.subplot(133)
+plt.plot(names, values)
+plt.suptitle('Categorical Plotting')
+plt.show()
