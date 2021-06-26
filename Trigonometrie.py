60f8b4a162447675bfb9a31326286727c9e452db
Kawa Noman <76186680+kawanoman@users.noreply.github.com>
2021-06-26 22:15:33 +0200

    Update Trigonometrie.py

--- Trigonometrie.py
+++ Trigonometrie.py
@@ -13,6 +13,9 @@ t2 = np.arange(0.0, 5.0, 0.02)
 plt.figure()
 plt.subplot(211)
 plt.plot(t1, f(t1), 'bo', t2, f(t2), 'k')
+plt.annotate('local max', xy=(1, 0.4), xytext=(1.5, 0.5),
+             arrowprops=dict(facecolor='black', shrink=0.05),
+             )
 plt.title('Easy as 1, 2, 3') # subplot 211 title
 plt.xlabel('Smarts')
 plt.ylabel('Probability')
