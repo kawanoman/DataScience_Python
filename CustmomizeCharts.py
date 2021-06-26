f916e912894222b7f2d0f0b258fc97f46d8175f5
Kawa Noman <76186680+kawanoman@users.noreply.github.com>
2021-06-26 21:37:57 +0200

    Create CustmomizeCharts.py

---
+++ CustmomizeCharts.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3, 4],[1, 4, 9, 16],'bs')
+#This is a comment for a solid line.
+#written in Python
+# axis decalres the limits of xand y labels.
+plt.axis([0, 6, 0, 20])
+plt.ylabel('some numbers')
+plt.xlabel('time')
+plt.title('Einstieg ins Thema')
+plt.show()
