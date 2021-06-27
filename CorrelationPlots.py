66ab7bc3f67fc8cc41fe510fd70038a7d20dde65
Kawa Noman <76186680+kawanoman@users.noreply.github.com>
2021-06-27 22:50:00 +0200

    Create CorrelationPlots.py

---
+++ CorrelationPlots.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = sns.load_dataset('iris')
+
+# left
+sns.pairplot(df, kind="scatter", hue="species", markers=["o", "s", "D"], palette="Set2")
+plt.show()
+
+# right: you can give other arguments with plot_kws.
+sns.pairplot(df, kind="scatter", hue="species", plot_kws=dict(s=80, edgecolor="white", linewidth=2.5))
+plt.show()
