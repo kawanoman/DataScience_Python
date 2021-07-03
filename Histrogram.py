dd842b4a86c5fbafee7be2d4ee3f39aeb493c758
Kawa Noman <76186680+kawanoman@users.noreply.github.com>
2021-07-03 10:49:41 +0200

    Update Histrogram.py

--- Histrogram.py
+++ Histrogram.py
@@ -4,9 +4,6 @@ import matplotlib.pyplot as plt
 # create random data
 names = 'groupA', 'groupB', 'groupC', 'groupD',
 values = [12, 11, 3, 30]
-
-
-
 # Label distance: gives the space between labels and the center of the pie
 plt.pie(values, labels=names, labeldistance=1.15);
 plt.show();
