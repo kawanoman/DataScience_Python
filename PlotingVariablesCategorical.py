6f64e8556bbf2e3cafd2074e7022d1674d216f8d
Kawa Noman <76186680+kawanoman@users.noreply.github.com>
2021-07-06 11:41:09 +0200

    Update PlotingVariablesCategorical.py

--- PlotingVariablesCategorical.py
+++ PlotingVariablesCategorical.py
@@ -2,9 +2,7 @@ import matplotlib.pyplot as plt
 
 names = ['group_a', 'group_b', 'group_c']
 values = [1, 10, 100]
-
 plt.figure(figsize=(8, 3))
-
 plt.subplot(131)
 plt.bar(names, values)
 plt.subplot(132)
